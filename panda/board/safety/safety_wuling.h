7b68d8baddb6d6e69a8995b12b4977f5fa45526e
Joes Lie <liejoes1+github@gmail.com>
2024-07-15 20:53:03 +0700

    c code remove unsused code

--- panda/board/safety/safety_wuling.h
+++ panda/board/safety/safety_wuling.h
@@ -1,29 +1,29 @@
-int wuling_rx_hook(CANPacket_t *to_push) {
-  UNUSED(to_push);
-  controls_allowed = 1;
-  return true;
-}
+// int wuling_rx_hook(CANPacket_t *to_push) {
+//   UNUSED(to_push);
+//   controls_allowed = 1;
+//   return true;
+// }
 
-static int wuling_tx_hook(CANPacket_t *to_send) {
-  UNUSED(to_send);
-  return true;
-}
+// static int wuling_tx_hook(CANPacket_t *to_send) {
+//   UNUSED(to_send);
+//   return true;
+// }
 
 
-static int wuling_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
-  UNUSED(to_fwd);
-  int bus_fwd = -1;
+// static int wuling_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
+//   UNUSED(to_fwd);
+//   int bus_fwd = -1;
   
-  if (bus_num == 0) {
-    bus_fwd = 2;
-  }
+//   if (bus_num == 0) {
+//     bus_fwd = 2;
+//   }
 
-  if (bus_num == 2) {
-    bus_fwd = 0;
-  }
+//   if (bus_num == 2) {
+//     bus_fwd = 0;
+//   }
 
-  return bus_fwd;
-}
+//   return bus_fwd;
+// }
 
 const safety_hooks wuling_hooks = {
   // .init = nooutput_init,
