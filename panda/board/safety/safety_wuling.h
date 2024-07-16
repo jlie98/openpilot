d0ec5ca8f8502833795829d5216dbdfb9240edab
Joes Lie <liejoes1+github@gmail.com>
2024-07-16 12:17:47 +0700

    Revert "change mazda -> wuling"
    This reverts commit edf2218bc7f44653ae7b23f21da5c2294e98c763.

--- panda/board/safety/safety_wuling.h
+++ panda/board/safety/safety_wuling.h
@@ -1,15 +1,16 @@
 // CAN msgs we care about
-//safety wuling
-#define ENGINE_DATA   0xc9
-#define LKAS_HUD      0x373
-#define STEERING_LKAS      0x225
-#define BRAKE_DATA      0x269
-#define GAS_DATA      0x260
+#define WULING_LKAS          0x243
+#define WULING_LKAS_HUD      0x440
+#define WULING_CRZ_CTRL      0x21c
+#define WULING_CRZ_BTNS      0x09d
+#define WULING_STEER_TORQUE  0x240
+#define WULING_ENGINE_DATA   0x202
+#define WULING_PEDALS        0x165
 
 // CAN bus numbers
-#define BUS_MAIN 0
-#define BUS_RADAR  1
-#define BUS_CAM  2
+#define WULING_MAIN 0
+#define WULING_AUX  1
+#define WULING_CAM  2
 
 #define WULING_MAX_STEER 2048U
 
@@ -23,41 +24,146 @@
 #define WULING_DRIVER_TORQUE_FACTOR 1
 #define WULING_MAX_TORQUE_ERROR 350
 
-const CanMsg WULING_TX_MSGS[] = {{ENGINE_DATA, 0, 8}, {LKAS_HUD, 0, 8}};
+const CanMsg WULING_TX_MSGS[] = {{WULING_LKAS, 0, 8}, {WULING_CRZ_BTNS, 0, 8}, {WULING_LKAS_HUD, 0, 8}};
 
-AddrCheckStruct wl_addr_checks[] = {
-  {.msg = {{ENGINE_DATA, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{BRAKE_DATA, 0, 8, .expected_timestep = 50000U}, { 0 }, { 0 }}},
-  {.msg = {{GAS_DATA, 0, 8, .expected_timestep = 50000U}, { 0 }, { 0 }}},
+AddrCheckStruct WULING_addr_checks[] = {
+  {.msg = {{WULING_CRZ_CTRL,     0, 8, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{WULING_CRZ_BTNS,     0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{WULING_STEER_TORQUE, 0, 8, .expected_timestep = 12000U}, { 0 }, { 0 }}},
+  {.msg = {{WULING_ENGINE_DATA,  0, 8, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{WULING_PEDALS,       0, 8, .expected_timestep = 20000U}, { 0 }, { 0 }}},
 };
+#define WULING_ADDR_CHECKS_LEN (sizeof(WULING_addr_checks) / sizeof(WULING_addr_checks[0]))
+addr_checks WULING_rx_checks = {WULING_addr_checks, WULING_ADDR_CHECKS_LEN};
 
-#define WL_RX_CHECK_LEN (sizeof(wl_addr_checks) / sizeof(wl_addr_checks[0]))
-addr_checks wl_rx_checks = {wl_addr_checks, WL_RX_CHECK_LEN};
 // track msgs coming from OP so that we know what CAM msgs to drop and what to forward
-
 static int WULING_rx_hook(CANPacket_t *to_push) {
-  bool valid = addr_safety_check(to_push, &wl_rx_checks, NULL, NULL, NULL);
+  bool valid = addr_safety_check(to_push, &WULING_rx_checks, NULL, NULL, NULL);
+  if (valid && ((int)GET_BUS(to_push) == WULING_MAIN)) {
+    int addr = GET_ADDR(to_push);
+
+    if (addr == WULING_ENGINE_DATA) {
+      // sample speed: scale by 0.01 to get kph
+      int speed = (GET_BYTE(to_push, 2) << 8) | GET_BYTE(to_push, 3);
+      vehicle_moving = speed > 10; // moving when speed > 0.1 kph
+    }
+
+    if (addr == WULING_STEER_TORQUE) {
+      int torque_driver_new = GET_BYTE(to_push, 0) - 127U;
+      // update array of samples
+      update_sample(&torque_driver, torque_driver_new);
+    }
+
+    // enter controls on rising edge of ACC, exit controls on ACC off
+    if (addr == WULING_CRZ_CTRL) {
+      bool cruise_engaged = GET_BYTE(to_push, 0) & 0x8U;
+      if (cruise_engaged) {
+        if (!cruise_engaged_prev) {
+          controls_allowed = 1;
+        }
+      } else {
+        controls_allowed = 0;
+      }
+      cruise_engaged_prev = cruise_engaged;
+    }
+
+    if (addr == WULING_ENGINE_DATA) {
+      gas_pressed = (GET_BYTE(to_push, 4) || (GET_BYTE(to_push, 5) & 0xF0U));
+    }
+
+    if (addr == WULING_PEDALS) {
+      brake_pressed = (GET_BYTE(to_push, 0) & 0x10U);
+    }
+
+    generic_rx_checks((addr == WULING_LKAS));
+  }
   return valid;
 }
 
 static int WULING_tx_hook(CANPacket_t *to_send) {
-  UNUSED(to_send);
-  return 1;
+  int tx = 1;
+  int addr = GET_ADDR(to_send);
+  int bus = GET_BUS(to_send);
+
+  if (!msg_allowed(to_send, WULING_TX_MSGS, sizeof(WULING_TX_MSGS)/sizeof(WULING_TX_MSGS[0]))) {
+    tx = 0;
+  }
+
+  // Check if msg is sent on the main BUS
+  if (bus == WULING_MAIN) {
+
+    // steer cmd checks
+    if (addr == WULING_LKAS) {
+      int desired_torque = (((GET_BYTE(to_send, 0) & 0x0FU) << 8) | GET_BYTE(to_send, 1)) - WULING_MAX_STEER;
+      bool violation = 0;
+      uint32_t ts = microsecond_timer_get();
+
+      if (controls_allowed) {
+
+        // *** global torque limit check ***
+        violation |= max_limit_check(desired_torque, WULING_MAX_STEER, -WULING_MAX_STEER);
+
+        // *** torque rate limit check ***
+        violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
+                                        WULING_MAX_STEER, WULING_MAX_RATE_UP, WULING_MAX_RATE_DOWN,
+                                        WULING_DRIVER_TORQUE_ALLOWANCE, WULING_DRIVER_TORQUE_FACTOR);
+
+        // used next time
+        desired_torque_last = desired_torque;
+
+        // *** torque real time rate limit check ***
+        violation |= rt_rate_limit_check(desired_torque, rt_torque_last, WULING_MAX_RT_DELTA);
+
+        // every RT_INTERVAL set the new limits
+        uint32_t ts_elapsed = get_ts_elapsed(ts, ts_last);
+        if (ts_elapsed > ((uint32_t) WULING_RT_INTERVAL)) {
+          rt_torque_last = desired_torque;
+          ts_last = ts;
+        }
+      }
+
+      // no torque if controls is not allowed
+      if (!controls_allowed && (desired_torque != 0)) {
+        violation = 1;
+      }
+
+      // reset to 0 if either controls is not allowed or there's a violation
+      if (violation || !controls_allowed) {
+        desired_torque_last = 0;
+        rt_torque_last = 0;
+        ts_last = ts;
+      }
+
+      if (violation) {
+        tx = 0;
+      }
+    }
+
+    // cruise buttons check
+    if (addr == WULING_CRZ_BTNS) {
+      // allow resume spamming while controls allowed, but
+      // only allow cancel while contrls not allowed
+      bool cancel_cmd = (GET_BYTE(to_send, 0) == 0x1U);
+      if (!controls_allowed && !cancel_cmd) {
+        tx = 0;
+      }
+    }
+  }
+
+  return tx;
 }
 
 static int WULING_fwd_hook(int bus, CANPacket_t *to_fwd) {
-  // fwd from car to camera. also fwd certain msgs from camera to car
-  UNUSED(to_fwd);
   int bus_fwd = -1;
+  int addr = GET_ADDR(to_fwd);
 
-  if (bus == BUS_MAIN) {
-    bus_fwd = BUS_CAM;
-  } else if (bus == BUS_CAM) {
-    // bool block = (addr == LKAS_HUD) || (addr == STEERING_LKAS);
-    // bool block =  (addr == STEERING_LKAS) || (addr == BRAKE_DATA)  || (addr == GAS_DATA);
-    // if (!block) {
-      bus_fwd = BUS_MAIN;
-  
+  if (bus == WULING_MAIN) {
+    bus_fwd = WULING_CAM;
+  } else if (bus == WULING_CAM) {
+    bool block = (addr == WULING_LKAS) || (addr == WULING_LKAS_HUD);
+    if (!block) {
+      bus_fwd = WULING_MAIN;
+    }
   } else {
     // don't fwd
   }
@@ -67,9 +173,9 @@ static int WULING_fwd_hook(int bus, CANPacket_t *to_fwd) {
 
 static const addr_checks* WULING_init(int16_t param) {
   UNUSED(param);
-  controls_allowed = 1;
+  controls_allowed = false;
   relay_malfunction_reset();
-  return &wl_rx_checks;
+  return &WULING_rx_checks;
 }
 
 const safety_hooks wuling_hooks = {
