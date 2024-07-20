5ec9dc34ba82e88439f7d02621ee2dd423af9720
Joes Lie <liejoes1+github@gmail.com>
2024-07-20 20:52:46 +0700

    disable joystick

--- cereal/services.py
+++ cereal/services.py
@@ -68,7 +68,7 @@ services = {
   "navThumbnail": (True, 0.),
 
   # debug
-  "testJoystick": (True, 0.),
+  "testJoystick": (False, 0.),
 }
 service_list = {name: Service(new_port(idx), *vals) for  # type: ignore
                 idx, (name, vals) in enumerate(services.items())}
