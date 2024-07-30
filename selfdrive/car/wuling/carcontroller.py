3aa2c4c1f19181ff63231899a04235aa9445c205
Joes Lie <liejoes1+github@gmail.com>
2024-07-30 19:35:51 +0700

    use byd angle

--- selfdrive/car/wuling/carcontroller.py
+++ selfdrive/car/wuling/carcontroller.py
@@ -10,6 +10,16 @@ from cereal import car
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
+def apply_wuling_steer_angle_limits(apply_angle, actual_angle, v_ego):
+  # pick angle rate limits based on wind up/down
+  ANGLE_RATE_LIMIT_UP = 3       # maximum allow 150 degree per second, 100Hz loop means 1.5
+  ANGLE_RATE_LIMIT_DOWN = 3
+  
+  steer_up = actual_angle * apply_angle >= 0. and abs(apply_angle) > abs(actual_angle)
+  rate_limits = ANGLE_RATE_LIMIT_UP if steer_up else ANGLE_RATE_LIMIT_DOWN
+
+  return clip(apply_angle, actual_angle - rate_limits, actual_angle + rate_limits)
+
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.apply_steer_last = 0
@@ -22,6 +32,7 @@ class CarController():
     
     self.packer_pt = CANPacker(DBC[CP.carFingerprint]['pt'])
 
+
   def update(self, c, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert, hud_speed, left_line, right_line, left_lane_depart, right_lane_depart):
 
     P = self.params
@@ -29,7 +40,8 @@ class CarController():
     # Send CAN commands.
     can_sends = []
     
-    
+    apply_angle = apply_wuling_steer_angle_limits(actuators.steeringAngleDeg, CS.out.steeringAngleDeg, CS.out.vEgo)
+
     # Steering (50Hz)
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we just received the
     # next Panda loopback confirmation in the current CS frame.
@@ -37,22 +49,17 @@ class CarController():
       self.lka_steering_cmd_counter_last = CS.lka_steering_cmd_counter
     elif (frame % P.STEER_STEP) == 0:
       lkas_enabled = True
-      if lkas_enabled:
-        new_steer = int(round(actuators.steer * P.STEER_MAX))
-        apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
-        self.steer_rate_limited = new_steer != apply_steer
-      else:
-        apply_steer = 0
+      # if !lkas_enabled:
+      # else:
+      #   apply_angle = CS.out.steeringAngleDeg
         
-      self.apply_steer_last = apply_steer
-      
       idx = (CS.lka_steering_cmd_counter + 1) % 4
     
-      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_steer, idx, lkas_enabled))
+      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_angle, idx, lkas_enabled))
     
     new_actuators = actuators.copy()
-    new_actuators.steer = self.apply_steer_last / P.STEER_MAX
-
-    print('Steer :  %s' % self.apply_steer_last)
+    new_actuators.steeringAngleDeg = apply_angle
+    
+    print('Steer :  %s' % apply_angle)
 
     return new_actuators, can_sends
